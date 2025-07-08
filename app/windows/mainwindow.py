ca0f966ebfcf3564d6dcc71f4934e18608a13d8b
Rimjact <Rimjact@ya.ru>
2025-07-08 16:00:41 +0300

    Create save/load config functionality

--- app/windows/mainwindow.py
+++ app/windows/mainwindow.py
@@ -1,4 +1,6 @@
-from os import getenv
+import json
+
+from os import getenv, path
 
 from PyQt6.QtCore import Qt, QTimer
 
@@ -16,7 +18,17 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.DEFAULT_MPMISSIONS_PATH = f'{getenv('LOCALAPPDATA')}\Arma 3\MPMissionsCache'
+        self.CONFIG_FILE_PATH = 'pbo_sender.json'
+        self.DEFAULT_USER_CONFIG = {
+            'search_folder': f'{getenv('LOCALAPPDATA')}\\Arma 3\\MPMissionsCache',
+            'target_files_prefix': 'UTF',
+            'max_file_size_mb': 8,
+            'current_mode': 0,
+            'fixed_delay': 0.3,
+            'check_interval': 60
+        }
+
+        self.user_config = self.read_user_config()
 
         self.setWindowTitle('PBO Sender')
         self.setWindowIcon(QIcon('icon.png'))
@@ -36,7 +48,7 @@ class MainWindow(QMainWindow):
 
         # Status Layout
         status_layout = QHBoxLayout()
-        self.status_label = QLabel()
+        self.status_label = QLabel('Ожидание...')
         self.status_label.setAlignment(Qt.AlignmentFlag.AlignLeft)
         self.status_label.setStyleSheet('font-weight: bold;')
         status_layout.addWidget(self.status_label, 70)
@@ -58,8 +70,7 @@ class MainWindow(QMainWindow):
         # Folder Layout
         folder_layout = QHBoxLayout()
 
-        # TODO: view full path of search directory
-        self.folder_label = QLabel(f'Папка для проверки: {self.DEFAULT_MPMISSIONS_PATH}')
+        self.folder_label = QLabel(f'Папка для проверки: {self.user_config['search_folder']}')
         self.folder_label.setWordWrap(True)
         folder_layout.addWidget(self.folder_label, 1)
 
@@ -96,9 +107,9 @@ class MainWindow(QMainWindow):
         # Buttons Layout
         buttons_layout = QHBoxLayout()
 
-        self.save_button = QPushButton('Сохранить настройки')
-        self.save_button.clicked.connect(self.on_save_button_clicked)
-        buttons_layout.addWidget(self.save_button)
+        self.save_config_button = QPushButton('Сохранить конфигурацию')
+        self.save_config_button.clicked.connect(self.on_save_config_button_clicked)
+        buttons_layout.addWidget(self.save_config_button)
 
         self.send_button = QPushButton('Отправить сейчас')
         self.send_button.clicked.connect(self.on_send_button_clicked)
@@ -141,13 +152,17 @@ class MainWindow(QMainWindow):
 
 
     def on_browse_button_clicked(self):
-        """Обработчик события когда нажата кнопка выбора папки для поиска."""
+        """Обработчик события, когда нажата кнопка выбора папки для поиска."""
 
         self.show_browse_dialog()
 
 
-    def on_save_button_clicked(self):
-        pass
+    def on_save_config_button_clicked(self):
+        """Обработчик события, когда нажата кнопка сохранения конфигурации."""
+
+        self.disable_buttons()
+        self.save_user_config()
+        self.enable_buttons()
 
 
     def on_send_button_clicked(self):
@@ -163,15 +178,42 @@ class MainWindow(QMainWindow):
 
 
     def on_show_window_action_triggered(self):
-        """Обработчик события когда сработало действие на показ главного окна."""
+        """Обработчик события, когда сработало действие на показ главного окна."""
 
         self.show_main_window()
 
 
     def on_exit_action_triggered(self):
-        """Обработчик события когда сработало действие на выход из приложения."""
+        """Обработчик события, когда сработало действие на выход из приложения."""
 
         self.exit_from_app()
+
+
+    def read_user_config(self):
+        """Считывает данные из конфига пользователя в формате JSON."""
+
+        try:
+            if not path.exists(self.CONFIG_FILE_PATH):
+                return self.DEFAULT_USER_CONFIG
+
+            with open(self.CONFIG_FILE_PATH, 'r', encoding='utf-8') as config_file:
+                return json.load(config_file)
+        except Exception as e:
+            # TODO: logger
+            return self.DEFAULT_USER_CONFIG
+
+
+    def save_user_config(self):
+        """Записывает данные в файл конфига в формате JSON."""
+
+        try:
+            with open(self.CONFIG_FILE_PATH, 'w', encoding='utf-8') as config_file:
+                json.dump(self.user_config, config_file, indent=2, ensure_ascii=False)
+
+            self.status_label.setText('Настройки сохранены')
+        except Exception as e:
+            self.status_label.setText(f'Ошибка сохранения: {str(e)}')
+            # TODO: Logger
 
 
     def show_main_window(self):
@@ -190,8 +232,22 @@ class MainWindow(QMainWindow):
 
 
     def show_browse_dialog(self):
-        """Показывает диалог выбора папки с кэшом миссий."""
+        """Показывает диалог выбора папки с файлами .pbo миссий."""
 
-        folder = QFileDialog.getExistingDirectory(self, 'Выберите папку с кэшом миссий', self.DEFAULT_MPMISSIONS_PATH)
+        folder = QFileDialog.getExistingDirectory(self, 'Выберите папку с файлами .pbo миссий', self.user_config['search_folder'])
         if folder:
             self.folder_label.setText(f'Папка для проверки: {folder}')
+
+
+    def enable_buttons(self):
+        """Включает все кнопки."""
+
+        self.send_button.setDisabled(False)
+        self.save_config_button.setDisabled(False)
+
+
+    def disable_buttons(self):
+        """Выключает все кнопки."""
+
+        self.send_button.setDisabled(True)
+        self.save_config_button.setDisabled(True)
